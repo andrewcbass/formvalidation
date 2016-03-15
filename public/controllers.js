838b926cbdbf31b7e192e8ebfd4090fc37923534
andrewcbass <andrew.c.bass@gmail.com>
2016-03-14 22:33:39 -0700

    show error on

--- public/controllers.js
+++ public/controllers.js
@@ -6,14 +6,10 @@ app.controller('formCtrl', function($scope) {
 
   $scope.passwordRegex = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}";
 
-  $scope.sameSame = function() {
-    if($scope.user.password === $scope.user.confirm) {
-      $scope.match = false;
-      return;
-    }
-    else {
-      $scope.match = true;
-    }
+  $scope.formSubmit = function(valid) {
+    console.log(valid);
+
+    
   }
 
 });
@@ -23,5 +19,5 @@ app.controller('cardCtrl', function($scope) {
 
 
 
-  
+
 })
